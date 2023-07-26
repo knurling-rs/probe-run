69f7c5a2b562cd205a269c5ec165a4ee3e07bcf6
Johann Hemmann <johann.hemmann@code.berlin>
2023-07-26 14:58:57 +0200

    `tests::snapshot`: Pass args as slice

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -17,7 +17,7 @@ struct RunResult {
 /// Run `probe-run` with `args` and truncate the output.
 ///
 /// If `terminate` is `true`, the command gets terminated after a short timeout.
-fn run(args: &str, terminate: bool) -> RunResult {
+fn run(args: &[&str], terminate: bool) -> RunResult {
     let (mut reader, mut handle) = run_command(args);
 
     if terminate {
@@ -50,14 +50,18 @@ fn wait_and_terminate(handle: &Child) {
     .expect("cannot send ctrl-c");
 }
 
-fn run_command(args: &str) -> (os_pipe::PipeReader, Child) {
-    let cmd = format!("run -- --chip nRF52840_xxAA tests/test_elfs/{args} --shorten-paths");
+fn run_command(args: &[&str]) -> (os_pipe::PipeReader, Child) {
+    let mut cmd = vec!["run", "--", "--chip", "nRF52840_xxAA", "--shorten-paths"];
+    cmd.extend(&args[1..]);
+
+    let path = format!("tests/test_elfs/{}", args[0]);
+    cmd.push(path.as_str());
 
     // capture stderr and stdout while preserving line order
     let (reader, writer) = pipe().unwrap();
 
     let handle = Command::new("cargo")
-        .args(cmd.split(' '))
+        .args(cmd)
         .stdout(writer.try_clone().unwrap())
         .stderr(writer)
         .spawn()
@@ -94,7 +98,13 @@ fn truncate_output(probe_run_output: String) -> String {
 #[serial]
 #[ignore = "requires the target hardware to be present"]
 fn snapshot_test(#[case] args: &str, #[case] success: bool) {
-    let run_result = run(args, false);
+    // Arrange
+    let args = args.split(' ').collect::<Vec<_>>();
+
+    // Act
+    let run_result = run(args.as_slice(), false);
+
+    // Assert
     assert_eq!(success, run_result.exit_status.success());
     insta::assert_snapshot!(run_result.output);
 }
@@ -104,7 +114,13 @@ fn snapshot_test(#[case] args: &str, #[case] success: bool) {
 #[ignore = "requires the target hardware to be present"]
 #[cfg(target_family = "unix")]
 fn ctrl_c_by_user_is_reported_as_such() {
-    let run_result = run("silent-loop-rzcobs", true);
+    // Arrange
+    let args = &["silent-loop-rzcobs"];
+
+    // Act
+    let run_result = run(args, true);
+
+    // Assert
     assert!(!run_result.exit_status.success());
     insta::assert_snapshot!(run_result.output);
 }
